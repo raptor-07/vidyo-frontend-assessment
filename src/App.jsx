4dea63296a22885c384da78a179c31d6f360326f
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-11-10 18:55:58 +0530

    feat: import components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@
 /* eslint-disable no-unused-vars */
 
 import { useState, useRef } from "react";
+import LoadVideo from "./components/LoadVideo";
+import WaveSurferPlayer from "./components/WaveSurferPlayer";
+import CanvasVideoLoader from "./components/CanvasVideoLoader";
+import RenderVideo from "./components/RenderVideo";
 import "./styles/App.css";
 
 function App() {
